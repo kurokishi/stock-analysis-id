9426897f63ee3392e48d2d25c14b4b4b4403b3e9
kurokishi <kuroreidho@gmail.com>
2025-06-01 18:16:42 +0700

    Create __init__.py

---
+++ views/__init__.py
@@ -0,0 +1,11 @@
+from .dashboard import DashboardView
+from .analysis import AnalysisView
+from .prediction import PredictionView
+from .components import StockComponents
+
+__all__ = [
+    'DashboardView',
+    'AnalysisView',
+    'PredictionView',
+    'StockComponents'
+]
