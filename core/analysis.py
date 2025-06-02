c9dad71a2d2cdb46e95b7f521d38399f67357fb7
kurokishi <kuroreidho@gmail.com>
2025-06-02 10:25:02 +0700

    Update analysis.py

--- core/analysis.py
+++ core/analysis.py
@@ -1,3 +1,7 @@
+from models.stock import StockData, StockInfo  # Tambahkan ini
+from models.prediction import PredictionResult
+import pandas as pd
+import numpy as np
 import numpy as np
 import pandas as pd
 from models.stock import StockData
