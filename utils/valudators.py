0a105bea49601c0f5d68d4f4ecfe57294ab0c57c
kurokishi <kuroreidho@gmail.com>
2025-06-02 10:25:37 +0700

    Delete utils/valudators.py

--- utils/valudators.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def validate_ticker(ticker: str) -> bool:
-    return isinstance(ticker, str) and len(ticker.split('.')) == 2
-
-def validate_date_range(start_date, end_date) -> bool:
-    return start_date < end_date if start_date and end_date else False
