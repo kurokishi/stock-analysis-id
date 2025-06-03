86a97b087f332cd7992dcb300230fd78df35e316
kurokishi <kuroreidho@gmail.com>
2025-06-03 22:24:58 +0700

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,11 +1,18 @@
-from .dashboard import DashboardView
-from .analysis import AnalysisView
-from .prediction import PredictionView
-from .components import StockComponents
+# views/__init__.py
+from .dashboard_view import show_dashboard
+from .fundamental_view import show_fundamental_analysis
+from .technical_view import show_technical_analysis
+from .prediction_view import show_price_prediction
+from .portfolio_view import portfolio_simulation
+from .comparison_view import compare_stocks
+from .news_sentiment import get_news_sentiment
 
 __all__ = [
-    'DashboardView',
-    'AnalysisView',
-    'PredictionView',
-    'StockComponents'
+    'show_dashboard',
+    'show_fundamental_analysis',
+    'show_technical_analysis',
+    'show_price_prediction',
+    'portfolio_simulation',
+    'compare_stocks',
+    'get_news_sentiment'
 ]
