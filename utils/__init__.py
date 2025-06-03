4ccec34739ea496becac21481f5ae5f9f5068095
kurokishi <kuroreidho@gmail.com>
2025-06-03 22:45:16 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,10 @@
 # utils/__init__.py
-from .formatter import format_rupiah  # Sesuaikan dengan nama file yang ada
-from .validator import validate_ticker, validate_date_range
+from .formatter import format_rupiah
+from .validator import StockValidator  # Impor kelasnya, bukan fungsi langsung
 from .data_fetcher import DataFetcher
 
 __all__ = [
     'format_rupiah',
-    'validate_ticker',
-    'validate_date_range',
+    'StockValidator',  # Tambahkan ke __all__
     'DataFetcher'
 ]
